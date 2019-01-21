9b5cd49daf71381329f45546b15cb77d7f170ba5
fsantvoord <45536843+fsantvoord@users.noreply.github.com>
2019-01-21 14:57:55 +0100

    Eindversie V2 - Feedback verwerkt
    *:focus {
    display: none; (WEG gehaald en op de juiste manier gestyled)
    } 
    
    Microinteractie voor inklappen formulier

--- script.js
+++ script.js
@@ -1,14 +1,87 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
-var downButton = document.querySelectorAll('article>button');
+var downButtons = document.querySelectorAll('article>button');
 
 
 
-downButton.forEach(function(downButton){
-    downButton.addEventListener("click", function(){
-        downButton.classList.toggle('download');
-        downButton.classList.toggle('klaar');    
-            });
-        
+downButtons.forEach(function (downButtons) {
+    downButtons.addEventListener("click", function () {
+        downButtons.classList.toggle('download');
+        downButtons.classList.toggle('klaar');
     });
+
+});
+
+
+var knop = document.getElementById('filterknop');
+console.log('knop gevonden', knop);
+
+var form = document.querySelector('form');
+console.log('form gevonden', form);
+
+knop.addEventListener('click', function() {
+    if (form.classList.contains('ingeklapt')) {
+        console.log('hij gaat uitklappen');
+        form.classList.add('omlaag');
+        form.addEventListener('animationend', function(){
+            form.classList.remove('omlaag');
+            setTimeout(1000);
+            form.classList.remove('ingeklapt');
+        })
+
+    }
+    else {
+        console.log('hij gaat inklappen');
+        form.classList.add('omhoog');
+        form.addEventListener('animationend', function (){
+            form.classList.remove('omhoog');
+            setTimeout(1000);
+            form.classList.add('ingeklapt');
+        })
+
+    }
+
+});
+
+
+//function() {
+//    if (form.classList.contains('ingeklapt')) {
+//        console.log('hij gaat uitklappen');
+//        form.classlist.add('omlaag');
+//        form.addEventListener('animationend', function(){
+//            form.classList.remove('omlaag');
+//            setTimeout(1000);
+//            form.classlist.remove('ingeklapt');
+//        })
+//
+//    }
+//    else {
+//        console.log('hij gaat inklappen');
+//        form.classList.add('omhoog');
+//        form.addEventListener('animationend', function (){
+//            form.classList.remove('omhoog');
+//            setTimeout(1000);
+//            form.classList.add('ingeklapt');
+//        })
+//
+//    }
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
